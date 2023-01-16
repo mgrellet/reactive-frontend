86decca535666098c83e2f0cb4a2edaa6bded26a
Martin Grellet <martin.alejandro.grellet@gmail.com>
2023-01-16 05:52:18 -0300

    front end base template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'reactive-frontend';
+
+  constructor(private http: HttpClient) {
+  }
+
+  private baseUrl: string = 'http://localhost:8080';
+  private reservationUrl: string = this.baseUrl + '/room/v1/reservation/';
+  rooms: Room[] | undefined;
+
+  ngOnInit(): void {
+    this.rooms = [
+      new Room("127", "127", "150"),
+      new Room("138", "138", "130"),
+      new Room("254", "254", "180")
+    ]
+  }
+}
+
+export class Room {
+  id: string;
+  roomNumber: string;
+  price: string;
+
+  constructor(id: string, roomNumber: string, price: string) {
+    this.id = id;
+    this.roomNumber = roomNumber;
+    this.price = price;
+
+  }
 }
