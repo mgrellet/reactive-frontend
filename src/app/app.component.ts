334e50791724108097b6effdab4eb75038cd601e
Martin Grellet <martin.alejandro.grellet@gmail.com>
2023-01-18 07:16:23 -0300

    create reservation & service with rest call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {ReservationRequest, ReservationService} from "./reservation.service";
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -9,19 +11,50 @@ import {HttpClient} from "@angular/common/http";
 export class AppComponent implements OnInit {
   title = 'reactive-frontend';
 
-  constructor(private http: HttpClient) {
+  constructor(private reservationService: ReservationService) {
   }
 
-  private baseUrl: string = 'http://localhost:8080';
-  private reservationUrl: string = this.baseUrl + '/room/v1/reservation/';
   rooms: Room[] | undefined;
+  roomSearchForm!: FormGroup;
+
+  currentCheckinValue!: string;
+  currentCheckoutValue!: string;
+  currentRoomNumber!: number;
+  currentPrice!: number;
 
   ngOnInit(): void {
+    this.roomSearchForm = new FormGroup({
+      checkin: new FormControl(''),
+      checkout: new FormControl(''),
+      roomNumber: new FormControl(''),
+      price: new FormControl('')
+    });
+
+    //Every time that en element change
+    this.roomSearchForm.valueChanges.subscribe(form => {
+      this.currentCheckinValue = form.checkin;
+      this.currentCheckoutValue = form.checkout;
+
+      if (form.roomNumber) {
+        let roomValues: string[] = form.roomNumber.split('|');
+        this.currentRoomNumber = Number(roomValues[0]);
+        this.currentPrice = Number(roomValues[1]);
+      }
+
+
+    });
+
     this.rooms = [
       new Room("127", "127", "150"),
       new Room("138", "138", "130"),
       new Room("254", "254", "180")
     ]
+  }
+
+  createReservation() {
+    this.reservationService.createReservation(
+      new ReservationRequest(this.currentRoomNumber, this.currentCheckinValue, this.currentCheckoutValue, this.currentPrice)
+    ).subscribe(result => console.log(result));
   }
 }
 
